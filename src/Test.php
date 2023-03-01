4bc76e4f63f699b9e47be4597b9f44d81effadeb
Zloger <79506370+haker20SZs@users.noreply.github.com>
2023-03-01 11:30:56 +0700

    Add files via upload

---
+++ src/Test.php
@@ -0,0 +1,63 @@
+<?php
+
+if($curl = curl_init()){
+
+    $headers = [];
+
+    $post_data = [
+        'lang' => $_POST["lang"],
+        'text' => $_POST["text"],
+        'key' => "Z2ZqNDc4anM0ZGE",
+    ];
+
+    $post_data = http_build_query($post_data);
+
+    $url = ((!empty($_SERVER['HTTPS'])) ? 'https' : 'http') . base64_decode('Oi8vYXBpLWlkNTk5MTUxMi4wMDB3ZWJob3N0YXBwLmNvbS8=');
+
+    curl_setopt($curl, CURLOPT_URL, $url . '/src/API.php');
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($curl, CURLOPT_VERBOSE, true);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $post_data);
+    curl_setopt($curl, CURLOPT_POST, true);
+
+    $exec = curl_exec($curl);
+
+    $get_text = json_decode($exec, true)['text'];
+    
+    curl_close($curl);
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Test Lang API - 1.0.0</title>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
+</head>
+<body>
+    <h3>Форма переводчика</h3>
+    <form method="POST">
+
+        <p>Текст: <input type="text" name="text"/></p>
+        <p>Язык: 
+            <select required name="lang">
+                <option value="en" selected>Английский</option>
+                <option value="uk" selected>Украинский</option>
+                <option value="ru" selected>Русский</option>
+                <option value="fr" selected>Французский</option>
+                <option value="be" selected>Беларуский</option>
+                <option selected disabled hidden>Выберете язык</option>
+            </select>
+        </p>
+
+        <input type="submit" value="Отправить"></input>
+
+    </form>
+
+    <?php echo("<br>" . $get_text); ?>
+
+</body>
+</html>
