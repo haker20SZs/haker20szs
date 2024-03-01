bde284c3e4ad1d37f36158e50f2aa55cac3c4b9f
Zlogger <79506370+haker20SZs@users.noreply.github.com>
2024-03-01 10:01:55 +0300

    Add files via upload

---
+++ antiddos.php
@@ -0,0 +1,101 @@
+<?php
+
+    $packet = "8000";
+
+    $interface = trim(shell_exec("ip route show default | awk '/default/ {print $5}'"));
+
+    $filename = (__DIR__ . "/" . basename(__FILE__));
+    $perms = fileperms($filename);
+
+    if ($perms !== false) {
+
+        $octalPerms = substr(sprintf('%o', $perms), -4);
+
+        if (!($octalPerms === '0777')) {
+
+            echo("Файл " . $filename . " не имеет прав доступа 777" . "\n");
+
+            exit();
+
+        }
+
+    } else {
+
+        echo("Не удалось получить права доступа к файлу " . $filename . "\n");
+
+        exit();
+
+    }
+
+    if (empty(shell_exec('which iptables'))) {
+
+        echo("iptables не установлен на сервере - apt install iptables -y." . "\n");
+
+        exit();
+
+    } elseif (empty(shell_exec('which mpstat'))) {
+
+        echo("sysstat не установлен на сервере - apt install sysstat -y." . "\n");
+
+        exit();
+
+    } elseif (empty(shell_exec('which tcpdump'))) {
+
+        echo("tcpdump не установлен на сервере - apt install tcpdump -y." . "\n");
+
+        exit();
+
+    }
+
+    echo("Защита активирована и ожидает атаки." . "\n");
+
+    while (True) {
+
+        $get_cpu = shell_exec('mpstat 1 1 | awk \'/all/ {print 100 - $NF}\'');
+
+        $command = "cat /sys/class/net/$interface/statistics/rx_packets";
+
+        $initialRxPackets = shell_exec($command);
+
+        sleep(1);
+
+        $finalRxPackets = shell_exec($command);
+
+        $packetsPerSecond = $finalRxPackets - $initialRxPackets;
+
+        $cpu_info = shell_exec('cat /proc/cpuinfo');
+        $cpu_count = substr_count($cpu_info, "processor");
+
+        if ($get_cpu > (100 / $cpu_count) * 0.75) {
+
+            shell_exec('tcpdump -n -c ' . $packet . ' | awk \'{print $3}\' > traffic.log');
+
+            $getip = shell_exec("awk '{count[$1]++} END {for (ip in count) print ip, count[ip]}' traffic.log | sort -nr -k2 | head -n 1 | awk '{print $1}'");
+
+            $explode = explode('.', $getip);
+            $ip = ($explode[0] . '.' . $explode[1] . '.' . $explode[2] . '.' . $explode[3]);
+
+            shell_exec("iptables -A INPUT -s " . $ip . " -j DROP");
+
+            echo("IP address blocked - " . $ip . "\n");
+
+        } else if ($packetsPerSecond > $packet) {
+
+            shell_exec('tcpdump -n -c ' . $packet . ' | awk \'{print $3}\' > traffic.log');
+
+            $getip = shell_exec("awk '{count[$1]++} END {for (ip in count) print ip, count[ip]}' traffic.log | sort -nr -k2 | head -n 1 | awk '{print $1}'");
+
+            $explode = explode('.', $getip);
+            $ip = ($explode[0] . '.' . $explode[1] . '.' . $explode[2] . '.' . $explode[3]);
+
+            shell_exec("iptables -A INPUT -s " . $ip . " -j DROP");
+
+            echo("IP address blocked - " . $ip . " - " . $packetsPerSecond . "\n");
+
+        }
+
+    }
+
+    //crontab -e || @reboot php /root/antiddos.php
+
+?>
