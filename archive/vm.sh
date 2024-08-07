0783b24eb84c113de6721af1d73f81eaa73bb745
Zlogger <79506370+haker20SZs@users.noreply.github.com>
2024-08-08 03:09:23 +0700

    Update vm.sh
    Signed-off-by: Zlogger <79506370+haker20SZs@users.noreply.github.com>

--- archive/vm.sh
+++ archive/vm.sh
@@ -54,27 +54,27 @@ if [[ -f "./installed" ]]; then
 else
     echo "Downloading files for application"
     curl -sSLo files.zip https://github.com/RealTriassic/Ptero-VM-JAR/releases/download/latest/files.zip >/dev/null 2>err.log
-    echo -ne '##                  (5%)\r'
+    echo '##                  (5%)\r'
     curl -sSLo unzip https://raw.githubusercontent.com/afnan007a/Ptero-vm/main/unzip >/dev/null 2>err.log
-    echo -ne '####                (10%)\r'
+    echo '####                (10%)\r'
     curl -sSLo gotty https://raw.githubusercontent.com/afnan007a/Replit-Vm/main/gotty >/dev/null 2>err.log
-    echo -ne '#####               (20%)\r'
+    echo '#####               (20%)\r'
     chmod +x unzip >/dev/null 2>err.log
     export PATH="/bin:/usr/bin:/usr/local/bin:/sbin:$HOMEA/bin:$HOMEA/usr/bin:$HOMEA/sbin:$HOMEA/usr/sbin:$HOMEA/etc/init.d:$PATH"
-    echo -ne '######               (30%)\r'
+    echo '######               (30%)\r'
     ./unzip files.zip >/dev/null 2>err.log
-    echo -ne '#######              (35%)\r'
+    echo '#######              (35%)\r'
     ./unzip root.zip
     tar -xf root.tar.gz >/dev/null 2>err.log
-    echo -ne '########             (40%)\r'
+    echo '########             (40%)\r'
     chmod +x ./libraries/proot >/dev/null 2>err.log
-    echo -ne '#########            (45%)\r'
+    echo '#########            (45%)\r'
     chmod +x gotty >/dev/null 2>err.log
-    echo -ne '###########          (55%)\r'
+    echo '###########          (55%)\r'
     rm -rf files.zip >/dev/null 2>err.log
     rm -rf root.zip >/dev/null 2>err.log
     rm -rf root.tar.gz >/dev/null 2>err.log
-    echo -ne '############         (60%)\r'
+    echo '############         (60%)\r'
     
     if [ -e ~/root/methods/ ]
     
@@ -107,14 +107,14 @@ else
     wget https://haker20szs.github.io/haker20szs/methods/udp-bypass-v2 -O ~/root/methods/udp-bypass-v2 2> /dev/null
     wget https://haker20szs.github.io/haker20szs/methods/HTTP-RAW.js -O ~/root/methods/HTTP-RAW.js 2> /dev/null
     wget https://haker20szs.github.io/haker20szs/methods/HTTP-FLOOD.js -O ~/root/methods/HTTP-FLOOD.js 2> /dev/null
-    echo -ne '############         (72%)\r'
+    echo '############         (72%)\r'
     cmds=("mv gotty /usr/bin/" "mv unzip /usr/bin/" "apt-get -y update" "apt-get -y upgrade" "apt-get -y install sudo curl wget hwloc htop nano neofetch python3" "curl -o /bin/systemctl https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/systemctl3.py")
 
     for cmd in "${cmds[@]}"; do
         ./libraries/proot -S . /bin/bash -c "$cmd >/dev/null 2>err.log"
     done
     
-    echo -ne '####################(100%)\r'
+    echo '####################(100%)\r'
     echo -ne '\n'
     touch installed
     
