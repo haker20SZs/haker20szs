c366ed19a832a1d581b98512891927e3e5078308
Zlogger <79506370+haker20SZs@users.noreply.github.com>
2024-08-08 04:15:53 +0700

    Update vm.sh
    Signed-off-by: Zlogger <79506370+haker20SZs@users.noreply.github.com>

--- archive/vm.sh
+++ archive/vm.sh
@@ -98,7 +98,7 @@ else
 
     echo "Installing modules."
 
-    cmds=("mv gotty /usr/bin/" "mv unzip /usr/bin/" "apt-get --fix-broken install -y apt-utils && apt-get clean && apt-get -y update && apt-get -y upgrade")
+    cmds=("mv gotty /usr/bin/" "mv unzip /usr/bin/" "apt-get --fix-broken install -y apt-utils && apt-get clean && apt-get -y update && apt-get -y upgrade && clear")
 
     for cmd in "${cmds[@]}"; do
         ./libraries/proot -S . /bin/bash -c "$cmd >/dev/null 2>err.log"
