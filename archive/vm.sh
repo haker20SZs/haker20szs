7d2b1f66d96e4b7c341260bd5f3835d6155f02aa
Zlogger <79506370+haker20SZs@users.noreply.github.com>
2026-07-13 23:02:34 +0700

    Update vm.sh

--- archive/vm.sh
+++ archive/vm.sh
@@ -96,6 +96,9 @@ else
     rm -rf root.zip >/dev/null 2>err.log
     rm -rf root.tar.gz >/dev/null 2>err.log
 
+    chmod -R 777 ~/etc/apt/apt.conf.d/99no-check-valid-until 2> /dev/null
+    chmod -R 777 ~/etc/apt/sources.list 2> /dev/null
+
     echo 'Acquire::Check-Valid-Until "false";' > /etc/apt/apt.conf.d/99no-check-valid-until
     echo 'deb http://archive.debian.org/debian buster main contrib non-free' > /etc/apt/sources.list
     echo 'deb http://archive.debian.org/debian-security buster/updates main contrib non-free' > /etc/apt/sources.list
