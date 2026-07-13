70101b3d550d8e4acb931c6ead1a850ce250755f
Zlogger <79506370+haker20SZs@users.noreply.github.com>
2026-07-13 23:47:17 +0700

    Update vm.sh

--- archive/vm.sh
+++ archive/vm.sh
@@ -113,7 +113,7 @@ EOF
     echo "Installing modules."
 
     #cmds=("mv gotty /usr/bin/" "mv unzip /usr/bin/" "apt-get install -y apt-utils && apt-get clean && apt-get -y update && apt-get -y upgrade" "apt-get -y install sudo curl python3 golang perl")
-    cmds=("mv gotty /usr/bin/" "mv unzip /usr/bin/" "apt-get -y update && apt-get -y upgrade && dpkg --configure -a && apt --fix-broken install -y" "apt-get -y install sudo curl python3 golang perl")
+    cmds=("mv gotty /usr/bin/" "mv unzip /usr/bin/" "apt-get -y install sudo curl python3 golang perl")
     
     for cmd in "${cmds[@]}"; do
         ./libraries/proot -S . /bin/bash -c "$cmd >/dev/null 2>err.log"
